c7b92e20f3350541572db50ba503ddbe33d1e529
cristian0497 <cristian04.97@hotmail.com>
2019-12-10 13:07:01 -0500

    [ADD] Randon numer -1000 && 1000, check Last Digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    number_conv = number * -1
+elif number > 0:
+    number_conv = number
+num = number_conv % 10
+if num > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, num))
+elif num == 0:
+    print("Last digit of {} is {} and is 0".format(number, num))
+elif num < 6 and not 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, num))
