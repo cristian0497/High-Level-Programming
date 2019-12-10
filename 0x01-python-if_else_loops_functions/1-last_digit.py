d5c5371647823c9737c7ac2c258bb069ec1d00ed
cristian0497 <cristian04.97@hotmail.com>
2019-12-10 13:28:56 -0500

    [FIX] add "and not 0"

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if num > 5:
     print(txt + "{} is {} and is greater than 5".format(number, num))
 elif num == 0:
     print(txt + "{} is {} and is 0".format(number, num))
-elif num < 6:
+elif num < 6 and not 0:
     print(txt + "{} is {} and is less than 6 and not 0".format(number, num))
