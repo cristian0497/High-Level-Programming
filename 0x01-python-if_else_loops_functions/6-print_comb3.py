e28a3b5790e57b5d32f4e586a8a151714a15329f
cristian0497 <cristian04.97@hotmail.com>
2019-12-10 16:25:38 -0500

    [ADD] print all posible different combinations of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+cont1 = 0; cont2 = 1
+y = cont2
+while (cont1 < 8):
+    while (y < 10):
+        print("{}{}".format(cont1, y), end=", ")
+        y += 1
+    y = cont2 + 1
+    cont2 += 1
+    cont1 += 1
+print("{}{}".format(cont1, cont2))
