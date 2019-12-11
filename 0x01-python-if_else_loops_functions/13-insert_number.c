39443d99b9c8cbeae70d4597cfa73df84c66536a
cristian0497 <cristian04.97@hotmail.com>
2019-12-10 21:55:23 -0500

    [FEATURE] Fix Betty style.

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * insert_node - funtion add new node
+ * @head: linked list
+ * @number: num to add
+ * Return: New linked list
  */
 listint_t *insert_node(listint_t **head, int number)
 {
@@ -10,17 +11,14 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *n;
 	int cont_pos = 0, cont;
 
-	while(number > tmp->n)
+	while (number > tmp->n)
 	{
-/*		printf("->%d %d\n", cont_pos, tmp->n); */
 		cont_pos++;
 		tmp = tmp->next;
 	}
-	tmp = *head; //n
+	tmp = *head;
 	for (cont = 0; cont < cont_pos - 1; cont++)
-	{
 		tmp = tmp->next;
-	}
 	n = tmp->next;
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -28,7 +26,5 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	tmp->next = new_node;
 	new_node->next = n;
-
-/*	printf(">>>%d %d\n", tmp->n ,cont_pos);*/
 	return (tmp);
 }
