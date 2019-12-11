9584b5c718e6ef11da5c523a74a647b7eb5aae15
cristian0497 <cristian04.97@hotmail.com>
2019-12-10 20:17:24 -0500

    [ADD] FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    cont = 1
+    while (cont < 100):
+        if cont % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif cont % 3 == 0:
+            print("Fizz", end=" ")
+        elif cont % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(cont, end=" ")
+        cont += 1
