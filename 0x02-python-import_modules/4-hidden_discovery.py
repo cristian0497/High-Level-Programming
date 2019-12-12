045004c9201843596f802d239a994934244b185a
cristian0497 <cristian04.97@hotmail.com>
2019-12-12 17:05:32 -0500

    [ADD] new File

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import hidden_4
+
+def main():
+    n = len(dir(hidden_4))
+    cont = 0
+    while cont < n:
+        print (dir(hidden_4))
+
+if __name__ == "__main__":
+    main()
