a3a679d610146efb2ea59ef868ad1c91b7412f8c
cristian0497 <cristian04.97@hotmail.com>
2019-12-12 15:37:57 -0500

    [FIX] wrong print values

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,15 @@ import sys
 
 
 def main():
-    n = len(sys.argv)
-    cont = 1
-    print("{:d} argument".format(n - 1) + ("." if n == 2 else "s"), end="")
-    print(":" if n > 2 else "")
+    n = len(sys.argv) - 1
+    cont = 0
+    print("{:d} argument".format(n) + (":" if n == 1 else "s"), end="")
+    if n == 0:
+        print(".", end="")
+    print(":" if n > 1 else "")
     while cont < n:
-        if n > 1:
-            print("{:d}: {}".format(cont, sys.argv[cont]))
+        if n > 0:
+            print("{:d}: {}".format(cont + 1, sys.argv[cont + 1]))
         cont += 1
 if __name__ == "__main__":
     main()
