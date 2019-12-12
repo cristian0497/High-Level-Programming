3d8bd17bfe369bc75025e1cf84679936cca17600
cristian0497 <cristian04.97@hotmail.com>
2019-12-12 14:17:47 -0500

    [FIX] string format to int nums, add __name__ = "__main__" main()

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,9 +5,9 @@ from calculator_1 import add, sub, mul, div
 def main():
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
-if 1:
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+if __name__ == "__main__":
     main()
