22f99773f05970f142165922ca553f0096b35df3
cristian0497 <cristian04.97@hotmail.com>
2019-12-12 15:21:27 -0500

    [FIX] structure,wrong print values

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,8 @@ import sys
 def main():
     n = len(sys.argv)
     cont = 1
-    print("{:d} argument".format(n - 1) + ("." if n == 2 else "s."))
+    print("{:d} argument".format(n - 1) + ("." if n == 2 else "s"), end="")
+    print(":" if n > 2 else "")
     while cont < n:
         if n > 1:
             print("{:d}: {}".format(cont, sys.argv[cont]))
