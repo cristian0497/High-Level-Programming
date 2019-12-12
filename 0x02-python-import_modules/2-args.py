367bb9089d2f45785f759b8a9b0d64aae0619f1c
cristian0497 <cristian04.97@hotmail.com>
2019-12-12 15:12:30 -0500

    [ADD] print Arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    n = len(sys.argv)
+    cont = 1
+    print("{:d} argument".format(n - 1) + ("." if n == 2 else "s."))
+    while cont < n:
+        if n > 1:
+            print("{:d}: {}".format(cont, sys.argv[cont]))
+        cont += 1
+if __name__ == "__main__":
+    main()
