745eb72bb7ad919101c92c93d3ae655690b6b880
cristian0497 <cristian04.97@hotmail.com>
2019-12-17 15:42:09 -0500

    [ADD] print list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/path
+def print_reversed_list_integer(my_list=[]):
+    cont = len(my_list)
+    for integer in my_list:
+        print("{:d}".format(my_list[cont - 1]))
+        cont -= 1
