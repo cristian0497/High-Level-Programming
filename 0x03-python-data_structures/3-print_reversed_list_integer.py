4fc193f881de114d83bfd0148201f50b1c23c5a7
cristian0497 <cristian04.97@hotmail.com>
2019-12-17 15:50:49 -0500

    [FIX] change if conditiona statement

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     cont = len(my_list)
-    if cont > 0 and my_list:
+    if cont > 0 and my_list is not None:
         for integer in my_list:
             print("{:d}".format(my_list[cont - 1]))
             cont -= 1
