f1d6039150517ec67e85343f4ae1fb33601a016d
cristian0497 <cristian04.97@hotmail.com>
2019-12-17 22:16:44 -0500

    [ADD] Print 2D array

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    x = 0
+    y = 0
+
+    while x < len(matrix):
+        while y < len(matrix[x]):
+            print("{}".format(matrix[x][y]), end="")
+            y += 1
+        print('')
+        x += 1
+        y = 0
