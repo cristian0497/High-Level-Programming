1b1064d48bd3de797c4eebbbe6f21234bc3ab6bf
cristian0497 <cristian04.97@hotmail.com>
2019-12-18 14:18:58 -0500

    [FIX] correction to structure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,9 +7,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) == 0:
         tuple_b = (0, 0)
     elif len(tuple_b) == 1:
-        tuble_b = (tuple_b[0], 0)
+        tuple_b = (tuple_b[0], 0)
 
     a = tuple_a[0] + tuple_b[0]
-    b = tuple_a[1] + tuple_b[0]
+    b = tuple_a[1] + tuple_b[1]
     new = (a, b)
     return (new)
