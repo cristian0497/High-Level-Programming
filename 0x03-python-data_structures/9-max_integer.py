4a0828f16346d0447892726db5c6a0f96b442a5f
cristian0497 <cristian04.97@hotmail.com>
2019-12-17 23:55:59 -0500

    [FIX] Max value, if conditional

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,8 @@ def max_integer(my_list=[]):
     ref = 0
     idx = 0
     cont = 0
+    if len(my_list) == 0:
+        return None
     for integer in my_list:
         if integer > ref:
             ref = integer
