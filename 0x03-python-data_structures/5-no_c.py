221593d7d7d282a316cc00041c75bf13cfdfe572
cristian0497 <cristian04.97@hotmail.com>
2019-12-18 18:20:36 -0500

    [ADD] string no C and C chars

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     st = ''
     for char in my_string:
-        if char is not 'cC':
-            st = ''.join(char)
+        if char != 'c' and char != 'C':
+            st += ''.join(char)
     return (st)
