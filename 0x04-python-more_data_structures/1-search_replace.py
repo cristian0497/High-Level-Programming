2e1a10f94d5884eea793eb5f041ff7dc160f3182
cristian0497 <cristian04.97@hotmail.com>
2019-12-19 14:10:47 -0500

    [FIX] change if statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,11 +2,11 @@
 def search_replace(my_list, search, replace):
     cont = 0
     tmp = my_list[:]
-    if not tmp:
-        return my_list
-    for x in my_list:
-        tmp[cont] = x
-        if cont == search - 1:
-            tmp[cont] = replace
-        cont += 1
-    return tmp
+    if my_list:
+        for x in my_list:
+            tmp[cont] = x
+            if cont == search - 1:
+                tmp[cont] = replace
+            cont += 1
+        return tmp
+    return None
