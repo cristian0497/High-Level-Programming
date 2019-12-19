23fb6a1b5620992da8515f5becf7e4f15b29db0e
cristian0497 <cristian04.97@hotmail.com>
2019-12-19 14:04:55 -0500

    [ADD] Change the X pisition for other value

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    cont = 0
+    tmp = my_list[:]
+    for x in my_list:
+        tmp[cont] = x
+        if cont == search - 1:
+            tmp[cont] = replace
+        cont += 1
+    return tmp
