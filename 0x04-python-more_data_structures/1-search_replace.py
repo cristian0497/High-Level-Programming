90201b2828010153f20680f0535330ecc665deb9
cristian0497 <cristian04.97@hotmail.com>
2019-12-19 14:07:08 -0500

    [FIX] If tmp is Null

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,8 @@
 def search_replace(my_list, search, replace):
     cont = 0
     tmp = my_list[:]
+    if not tmp:
+        return my_list
     for x in my_list:
         tmp[cont] = x
         if cont == search - 1:
