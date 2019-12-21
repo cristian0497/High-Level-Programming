dceb3bf19a5d3952846f9af10ffecdf9aa7caabf
cristian0497 <cristian04.97@hotmail.com>
2019-12-21 17:33:42 -0500

    [FIX] return fail

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,4 +3,5 @@ def weight_average(my_list=[]):
     if len(my_list) > 0:
         x = sum([a * b for a, b in my_list])
         y = sum([b for a, b in my_list])
-    return x / y
+        return x / y
+    return 0
