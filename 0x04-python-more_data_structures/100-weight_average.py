37caa28b9f585970e505979d67bad5394103c1fb
cristian0497 <cristian04.97@hotmail.com>
2019-12-21 17:31:25 -0500

    [FIX] if list null

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    x = sum([a * b for a, b in my_list])
-    y = sum([b for a, b in my_list])
+    if len(my_list) > 0:
+        x = sum([a * b for a, b in my_list])
+        y = sum([b for a, b in my_list])
     return x / y
