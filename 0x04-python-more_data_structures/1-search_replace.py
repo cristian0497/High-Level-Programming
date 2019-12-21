3f113b1f0b2b92cf49f6cd7a5c526492dbb3d952
cristian0497 <cristian04.97@hotmail.com>
2019-12-21 17:24:43 -0500

    [ADD] New version

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    cont = 0
-    tmp = my_list[:]
-    if my_list:
-        for x in my_list:
-            tmp[cont] = x
-            if cont == search - 1:
-                tmp[cont] = replace
-            cont += 1
-        return tmp
-    return None
+    tmp = [x if x is not search else replace for x in my_list]
+    return tmp
