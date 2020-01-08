8dc6c1e0af7bd6102590057ce816506c2320999a
cristian0497 <cristian04.97@hotmail.com>
2020-01-08 18:21:13 -0500

    [ADD] Pep8 OK, print div with 2 numbers

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+def safe_print_division(a, b):
+    ret = 0
+    try:
+        print("Inside result:", end="")
+        ret = a / b
+    except:
+        return None
+    finally:
+        print("{}".format(ret))
+    return ret
