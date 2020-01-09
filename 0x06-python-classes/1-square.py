d0df9357721f1de1351e14e451e1ce9374ab06e3
cristian0497 <cristian04.97@hotmail.com>
2020-01-09 16:23:44 -0500

    [ADD] PEP8 ok, Atrubutte private __size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module Class
+"""
+
+
+class Square:
+    """
+    Class Square
+    """
+    def __init__(self, size):
+        """
+        def func - size private
+        """
+        self.__size = size
