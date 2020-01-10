b6de141a1f5072ca14caef443e8c8a832570dbc4
cristian0497 <cristian04.97@hotmail.com>
2020-01-10 18:15:39 -0500

    [ADD] Print square, PEP8 OK

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+        new = ""
+        if self.__size != 0:
+            if self.__position[1] > 0:
+                new += "\n"
+                for i in range(self.__size):
+                    new += str(" " * self.__position[0])
+                    new += str("#" * self.__size + "\n")
+                new = new[:-1]
+            else:
+                for j in range(self.__size):
+                    new += str("#" * self.__size + "\n")
+                new = new[:-1]
+        return new
+
+    def my_print(self):
+        """ Print the square with size """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print(" " * self.position[0] + "#" * self.__size)
+
+    def area(self):
+        """ Return Area Square """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be and integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) and len(value) == 2:
+            if isinstance(value[0], int) and isinstance(value[1], int):
+                if value[0] >= 0 and value[1] >= 0:
+                    self.__position = value
+                    return
+        raise TypeError("position must be a tuple of 2 positive integers")
