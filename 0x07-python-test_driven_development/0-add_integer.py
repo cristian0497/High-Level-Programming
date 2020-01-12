c2b531e942f4c9fc76151270fe9368d3c971c2ab
cristian0497 <cristian04.97@hotmail.com>
2020-01-12 07:21:29 -0500

    [ADD] Return Addition of a and b, with .txt test file

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Method
+
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return the sum of a and b
+    """
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
