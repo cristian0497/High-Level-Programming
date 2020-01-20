8fc75cdb80ea46c2c836fb2de24ba9fea40de44b
cristian0497 <cristian04.97@hotmail.com>
2020-01-20 09:12:16 -0500

    [ADD] pep8 OK, return tru is isinstance, False if not

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Isinstance() of an object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Return True if is istance, False otherway
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
