a18a303d6228c875c34f05ad28909693547f55e3
cristian0497 <cristian04.97@hotmail.com>
2020-01-20 06:27:26 -0500

    [ADD] pep8 OK, Return a list of attributtes and methos of a class

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Return a list of available attributes and methods of an object:
+"""
+
+
+def lookup(obj):
+    """
+    available attributes and methods of an objects
+    """
+    return dir(obj)
