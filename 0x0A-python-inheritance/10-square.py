a483f144c43d149015bde8f6911b849f0dcad785
cristian0497 <cristian04.97@hotmail.com>
2020-01-21 04:10:18 -0500

    [ADD] class, sub class, and 3th subclass, pep8 ok

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Class Gemometry
+"""
+
+
+class BaseGeometry:
+    """
+    main class BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """
+    SubClass Rectangle
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    """
+    SubClass Square of rectangle
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
