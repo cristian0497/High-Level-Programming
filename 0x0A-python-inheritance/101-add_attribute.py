5998db3fe151297e10d859a1db79cc91330cbfc9
cristian0497 <cristian04.97@hotmail.com>
2020-01-22 10:26:07 -0500

    [FIX[ change the setattr for obj.__SETATTR__

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -9,5 +9,5 @@ def add_attribute(obj, at_name, attribute):
     Set attribute in a object specified
     """
     if hasattr(obj, at_name):
-        raise TypeError("can't add new attribute ")
-    setattr(obj, str(at_name), str(attribute))
+        raise TypeError("can't add new attribute")
+    obj.__setattr__(at_name, attribute)
