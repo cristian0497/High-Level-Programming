a2a01649573bc9b27bddf7f10a8f1df24e59cb11
cristian0497 <cristian04.97@hotmail.com>
2020-01-22 14:04:37 -0500

    [ADD] setattr to deci of object

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Return a student class dict
+"""
+
+
+class Student:
+    """
+    Class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if (attrs is None) or (type(attrs) != list):
+            return self.__dict__
+        ret = {}
+        for arg in attrs:
+            for key in self.__dict__:
+                if arg == key:
+                    ret[key] = (self.__dict__[key])
+        return ret
+
+    def reload_from_json(self, json):
+        for attr in json:
+            setattr(self, attr, json[attr])
