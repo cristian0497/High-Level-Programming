6abed6953d342781f2f3cb83497442e31a5fa5d5
cristian0497 <cristian04.97@hotmail.com>
2020-01-22 14:21:37 -0500

    [ADD] pep8 Ok, Print the Pascal Triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Pascal triangle
+"""
+
+
+def pascal_triangle(size):
+    """
+    Pascal Triangle
+    """
+    triangle = [[1], [1, 1]]
+    for x in range(1, size):
+        line = [1]
+        for y in range(0, len(triangle[x]) - 1):
+            line.extend([triangle[x][y] + triangle[x][y + 1]])
+        line += [1]
+        triangle.append(line)
+    return triangle
