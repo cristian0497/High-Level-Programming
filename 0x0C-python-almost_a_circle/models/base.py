18cc5b4344af31a6a1e0f9beea2bd8bd5e3820f1
cristian0497 <cristian04.97@hotmail.com>
2020-01-24 12:54:00 -0500

    [ADD] pep8 Ok, Task 16, save into a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,8 +18,22 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ dictionary python to Json string """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ save into a file json """
+        filename = (cls.__name__ + ".json")
+        with open(filename, mode="w") as f:
+            lis = []
+            if list_objs is None:
+                f.write(cls.to_json_string(lis))
+            else:
+                for it in range(0, len(list_objs)):
+                    lis.append(list_objs[it].to_dictionary())
+                f.write(cls.to_json_string(lis))
