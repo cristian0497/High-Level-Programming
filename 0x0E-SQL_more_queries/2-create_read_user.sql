64d815764e24108035d34ecea0e2286485b9008f
cristian0497 <cristian04.97@hotmail.com>
2020-03-03 19:10:27 +0000

    [ADD]

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
 -- Create A DataBase And User
 -- if not exists the script should not fail
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
