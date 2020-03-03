fb13682bc6bcb0311c9fe59cb0da1f272210d5d1
cristian0497 <cristian04.97@hotmail.com>
2020-03-03 03:59:03 +0000

    [ADD] Repeat values

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- print with AS function
 -- Number by score with AS attribute
-SELECT score, id AS number FROM second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, id AS number FROM second_table WHERE score IN (SELECT SCORE FROM second_table) ORDER BY score DESC;
