c1be952d76bc02b0814aec0b5d9a2bc4c0219fff
cristian0497 <cristian04.97@hotmail.com>
2020-03-03 04:25:08 +0000

    [ADD] count the n times repetitions of same score and numbre

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- print with AS function
 -- Number by score with AS attribute
-SELECT score, id AS number FROM second_table WHERE score IN (SELECT SCORE FROM second_table) ORDER BY score DESC;
+SELECT score, COUNT(score) FROM second_table GROUP BY score DESC;
