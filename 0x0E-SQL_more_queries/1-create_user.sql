4bcf9b964a6399d82b5fd2eaae5ab2ac52250f22
cristian0497 <cristian04.97@hotmail.com>
2020-03-03 15:12:49 +0000

    [ADD] create, if not exists a new user with All permissions

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Root User privileges
+-- Create a User woth ALL PRIVILEGES
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
