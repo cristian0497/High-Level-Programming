26f501ff3021a61a3ceb2cf6d78afa4f97335f70
cristian0497 <cristian04.97@hotmail.com>
2020-03-04 00:01:36 +0000

    [ADD] Id No null, Unique, autoincrement, primary key

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- States Table
+-- Creates DataBAse and data states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(256)); 
