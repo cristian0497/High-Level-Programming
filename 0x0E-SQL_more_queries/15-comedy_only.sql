060da6b04c1f6a9621c61f44affeef2bff07e280
cristian0497 <cristian04.97@hotmail.com>
2020-03-05 03:30:00 +0000

    [ADD] Comedy Only

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,7 @@
 -- Onlu Comedy
 -- List all Comedy showss in the database
-SELECT tv_shows.title FROM tv_genres JOIN tv_genres 
-ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.id = 5
+SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres 
+ON tv_show_genres.show_id = tv_shows.id 
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id 
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
