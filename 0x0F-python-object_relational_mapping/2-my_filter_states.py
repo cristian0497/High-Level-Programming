36e7bb499d5181509368c838254c06709a7c8e13
cristian0497 <cristian04.97@hotmail.com>
2020-03-28 20:46:47 +0000

    [ADD] Print only values requested

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Method """
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """ function to query in MySql server db witn name start N"""
+    query = MySQLdb.connect(host='localhost',
+                            user=argv[1],
+                            passwd=argv[2],
+                            database=argv[3],
+                            port=3306,
+                            charset='utf8')
+    cur = query.cursor()
+    cur.execute('SELECT id, name FROM states WHERE name = "{}"ORDER BY id ASC;'
+                .format(argv[4]))
+    cur2 = cur.fetchall()
+    for result in cur2:
+        print(result)
+    query.close()
+    cur.close()
+
+if __name__ == "__main__":
+    main()
