f9919e38d929fa11489a40fe02adb9db0723b7b7
cristian0497 <cristian04.97@hotmail.com>
2020-03-28 21:50:54 +0000

    [FIX] change structure, close() added

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,22 +4,17 @@ import MySQLdb
 from sys import argv
 
 
-def function():
-    """ fenction to query in MySql server db witn name start N"""
-    try:
-        query = MySQLdb.connect(host='localhost',
-                                user=argv[1],
-                                passwd=argv[2],
-                                db=argv[3],
-                                port=3306)
-        cur = query.cursor()
-        cur.execute("SELECT id, name FROM states WHERE name RLIKE '^N' ORDER BY\
-        states.id ASC")
-        cur2 = cur.fetchall()
-        for result in cur2:
-            print(result)
-
-    except:
-        pass
-if __name__ == '__main__':
-    function()
+if __name__ == "__main__":
+    query = MySQLdb.connect(host='localhost',
+                            user=argv[1],
+                            passwd=argv[2],
+                            db=argv[3],
+                            port=3306)
+    cur = query.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY\
+    states.id ASC")
+    cur2 = cur.fetchall()
+    for result in cur2:
+        print(result)
+    query.close()
+    cur.close()
