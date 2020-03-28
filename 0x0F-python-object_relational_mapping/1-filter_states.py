a3775446c803a03da8543167d0fa723636376e4b
cristian0497 <cristian04.97@hotmail.com>
2020-03-28 20:54:49 +0000

    [FEATURE] add a try and except structure

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,20 +6,23 @@ from sys import argv
 
 def main():
     """ fenction to query in MySql server db witn name start N"""
-    query = MySQLdb.connect(host='localhost',
-                            user=argv[1],
-                            passwd=argv[2],
-                            database=argv[3],
-                            port=3306,
-                            charset='utf8')
-    cur = query.cursor()
-    cur.execute("SELECT id, name FROM states WHERE name RLIKE '^N|^n' ORDER BY\
-    id ASC;")
-    cur2 = cur.fetchall()
-    for result in cur2:
-        print(result)
-    query.close()
-    cur.close()
+    try:
+        query = MySQLdb.connect(host='localhost',
+                                user=argv[1],
+                                passwd=argv[2],
+                                database=argv[3],
+                                port=3306,
+                                charset='utf8')
+        cur = query.cursor()
+        cur.execute("SELECT id, name FROM states WHERE name RLIKE '^N|^n' ORDER BY\
+        id ASC;")
+        cur2 = cur.fetchall()
+        for result in cur2:
+            print(result)
+        query.close()
+        cur.close()
+    except:
+        pass
 
 if __name__ == "__main__":
     main()
