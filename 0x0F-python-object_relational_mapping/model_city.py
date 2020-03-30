513a9fa67af013023a977a8f5bd6b3ec9ef5b7b9
cristian0497 <cristian04.97@hotmail.com>
2020-03-30 03:57:17 +0000

    [ADD] Mode city base

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 Base = declarative_base()
 
+
 class City(Base):
     """ Hybrid attributes for city_class """
     __tablename__ = "cities"
