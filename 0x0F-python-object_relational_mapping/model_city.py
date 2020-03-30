100edbb19835ee017872967638758baed2a70f2c
cristian0497 <cristian04.97@hotmail.com>
2020-03-30 04:18:09 +0000

    [FIX] from model_state added

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,7 +2,7 @@
 """ Base Model for City objects """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
-
+from model_state import Base
 
 Base = declarative_base()
 
