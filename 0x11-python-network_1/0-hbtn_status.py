da4d844ed54a06f7d105a8fb0a5ee51b93899273
cristian0497 <cristian04.97@hotmail.com>
2020-04-16 14:17:47 -0500

    [ADD]

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Status of a web Page """
+import urllib.request
+
+
+def main():
+    url = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(url) as response:
+        info = response.read()
+        print("Body response:")
+        print("\t - type: {}".format(type(response.read())))
+        print("\t - content: {}".format(info))
+        print("\t - utf8 content: {}".format(info.decode('utf-8')))
+
+if __name__ == "__main__":
+    main()
