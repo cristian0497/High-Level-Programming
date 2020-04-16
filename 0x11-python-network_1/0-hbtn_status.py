c5411996a543560ed1fe8dbb83651d4a82b2d58b
cristian0497 <cristian04.97@hotmail.com>
2020-04-16 21:19:01 +0000

    [ADD] change structure

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     url = urllib.request.Request('https://intranet.hbtn.io/status')
     with urllib.request.urlopen(url) as response:
         info = response.read()
-        print("Body response:")
-        print("\t - type: {}".format(type(response.read())))
-        print("\t - content: {}".format(info))
-        print("\t - utf8 content: {}".format(info.decode('utf-8')))
+    print("Body response:")
+    print("\t - type: {}".format(type(response.read())))
+    print("\t - content: {}".format(info))
+    print("\t - utf8 content: {}".format(info.decode('utf-8')))
