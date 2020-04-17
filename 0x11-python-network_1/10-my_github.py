5c2068ae8167b3b1e0cb37ea1e18efd9301b03c9
cristian0497 <cristian04.97@hotmail.com>
2020-04-17 04:44:04 +0000

    [ADD] Request to GitHub API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,7 +8,7 @@ def main():
     """ Request the info from GITHUB API """
     usr = sys.argv[1]
     pwd = sys.argv[2]
-    url = requests.get('https://api.github.com/user', auth = (usr, pwd))
+    url = requests.get('https://api.github.com/user', auth=(usr, pwd))
     dic = url.json()
     print(dic.get("id"))
 
