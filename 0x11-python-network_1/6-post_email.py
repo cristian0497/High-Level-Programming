43ffb2e58697ac190d2a0ed35c397b6a8e6962b0
cristian0497 <cristian04.97@hotmail.com>
2020-04-17 02:03:51 +0000

    [ADD] send and print request method package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Request package for python """
+"""Request package for python"""
 import requests
 import sys
 
 
 def main():
-    """ request and send a dictionary, print the request """
-    obj = {"email" : sys.argv[2]}
-    url = requests.post(sys.argv[1], data = obj)
+    """request and send a dictionary, print the request"""
+    obj = {"email": sys.argv[2]}
+    url = requests.post(sys.argv[1], data=obj)
     print(url.text)
 
 if __name__ == "__main__":
