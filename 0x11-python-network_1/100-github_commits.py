1da82a90a6f3162052c4c086af3570d49c5a00c8
cristian0497 <cristian04.97@hotmail.com>
2020-04-17 18:50:48 +0000

    [ADD] GitHub commits with

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,8 +9,8 @@ def main():
     st = "{}/{}".format(sys.argv[1], sys.argv[2])
     url = requests.get('https://api.github.com/repos/{}/commits'.format(st))
     response = url.json()
-    for x in response[:10]:
-        print("{}: {}".format(x['sha'], x['author']['login']))
+    for key in response[:10]:
+        print("{}: {}".format(key['sha'], key['commit']['author']['name']))
 
 if __name__ == "__main__":
     main()
