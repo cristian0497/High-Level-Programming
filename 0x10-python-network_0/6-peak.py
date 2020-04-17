ea6c7c8a745d5c670871d68be13e28655f04f7bc
cristian0497 <cristian04.97@hotmail.com>
2020-04-17 20:09:03 +0000

    [ADD]

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,10 +5,10 @@
 def find_peak(list_int):
     if len(list_int) > 0:
         y = 1
-        for x in range(0, len(list_int)):
+        for x in range(1, len(list_int) - 1):
             if list_int[x] >= list_int[x-1] and list_int[x] >= list_int[x + 1]:
                 return list_int[x]
         if list_int[y] > list_int[y + 1]:
             return list_int[y]
         else:
-            return list_int
+            return list_int[x]
