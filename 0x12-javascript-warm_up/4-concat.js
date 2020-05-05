698b86f983ff58f73cef46b5b689b677e5cc7846
Cristian <cristian@Blue>
2020-05-05 16:41:20 -0500

    [ADD] correct the format string

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-console.log(process.argv[2], 'is', process.argv[3]);
+const var1 = process.argv[2];
+const st = 'is';
+const var2 = process.argv[3];
+console.log('%s %s %s', var1, st, var2);
