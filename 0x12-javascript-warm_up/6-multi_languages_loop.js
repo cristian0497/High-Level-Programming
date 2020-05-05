0952141818aaa0c66263fe20b35337ecfdcabf47
Cristian <cristian@Blue>
2020-05-05 18:09:24 -0500

    [ADD] fix semistyle

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing']
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let x = 0;
 for (x in array) {
   console.log(array[x]);
-} 
+}
