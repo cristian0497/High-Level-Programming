1b9420c7ef0908f2f016f6fdd263742a47654035
Cristian <cristian@Blue>
2020-05-05 21:20:49 -0500

    [ADD]

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,10 @@
 const Maj = [];
 const len = process.argv.length - 2;
 if (len < 2) {
-  console.log('0');
+  console.log(0);
 } else {
   for (let x = 0; x < len; x++) {
-    Maj[x] = parseInt(process.argv[x + 2]);
+    Maj[x] = process.argv[x + 2];
   }
-  console.log('%s', Maj.sort()[len - 2]);
+  console.log(Maj.sort()[len - 2]);
 }
