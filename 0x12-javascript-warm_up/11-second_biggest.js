ee17016deb52f93e5c813d078962912fdc703c2b
Cristian <cristian@Blue>
2020-05-05 21:14:06 -0500

    [ADD] fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ if (len < 2) {
   console.log('0');
 } else {
   for (let x = 0; x < len; x++) {
-    Maj[x] = process.argv[x + 2];
+    Maj[x] = parseInt(process.argv[x + 2]);
   }
-  console.log(Maj.sort()[len - 2]);
+  console.log('%s', Maj.sort()[len - 2]);
 }
