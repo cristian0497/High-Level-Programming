6d713c7d42e183fafa7f15eac31ae2f54744788b
cristian0497 <cristian04.97@hotmail.com>
2020-05-12 12:55:03 -0500

    [ADD]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv.slice(2)[0], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
-    return;
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
