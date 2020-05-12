c0c8afe91bc88c2dd072c094e518f0ba4f8c01c4
cristian0497 <cristian04.97@hotmail.com>
2020-05-12 12:58:19 -0500

    [ADD] new structure

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv.slice(2)[0], 'utf8', (err, data) => {
+fs.readFile(process.argv.slice(2)[0], 'utf8', function(err, data) {
   if (err) {
     console.error(err);
   } else {
