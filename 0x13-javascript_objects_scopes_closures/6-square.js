0a97e36b5ad97bcb07fda23b6167d8964899dd89
cristian0497 <cristian04.97@hotmail.com>
2020-05-11 19:08:11 -0500

    [ADD] new structure

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
+const newSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
-  }
-
+class Square extends newSquare {
   charPrint (c) {
-    for (let x = 0; x < this.size; x++) {
-      console.log((c || 'X').repeat(parseInt(this.size)));
+    for (let x = 0; x < this.width; x++) {
+      console.log((c || 'X').repeat(parseInt(this.width)));
     }
   }
 }
+
 module.exports = Square;
