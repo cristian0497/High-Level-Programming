2b93559a466f9d37f1f6400241770c49f25c2118
cristian0497 <cristian04.97@hotmail.com>
2020-05-12 12:19:45 -0500

    [ADD] semistandar OK, read a file in utf-8

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 try {
   const info = fs.readFileSync(process.argv[2], 'utf-8');
   console.log(info);
-  } catch {
-    console.log(info);
-  }
+} catch (error) {
+  console.error(error);
+}
