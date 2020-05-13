7378d2dd2b3f336ce602bf93517bb43c630a4df8
cristian0497 <cristian04.97@hotmail.com>
2020-05-13 15:11:18 -0500

    [ADD

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (error, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const values = JSON.parse(body.body).characters;
+    for (let x = 0; x < values.length; x++) {
+      request.get(values[x], function (error, characters) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(characters.body).name);
+        }
+      });
+    }
+  }
+});
