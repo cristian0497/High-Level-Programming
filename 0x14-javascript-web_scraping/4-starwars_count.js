f91601095e2ebdda6dd758ff38104e3a79a29f01
cristian0497 <cristian04.97@hotmail.com>
2020-05-12 20:34:15 -0500

    [ADD] query to API with JavaScript

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/people/18';
+if (process.argv[2] === 'https://swapi-api.hbtn.io/api/films') {
+  request.get(url, function (error, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const full = JSON.parse(body.body);
+      console.log(full.films.length);
+    }
+  });
+}
